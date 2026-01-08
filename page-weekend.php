e2eaa477ed479c1123b6c4164ed2fd88882b3223
Bethany Fowler <beththewebbuilder@gmail.com>
2026-01-08 20:47:38 +0000

    add weekend page - ongoing

---
+++ page-weekend.php
@@ -0,0 +1,136 @@
+<?php
+get_header();
+?>
+
+<div class="floral-border-top">
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/floral-border-top.png"/>
+</div>
+
+<div class="weekend-top">
+    <div class="baskerville-regular capital-text sm-font">
+        Together with their families
+    </div>
+    <div class="alex-brush xl-font">
+        Brett Luffman
+    </div>
+    <div class="baskerville-regular capital-text sm-font">
+        and
+    </div>
+    <div class="alex-brush xl-font">
+        Bethany Fowler
+    </div>
+</div>
+
+<div class="baskerville-regular capital-text sm-font">
+    warmly invite you to celebrate their marriage over a wedding weekend
+</div>
+
+<div class="margin-1">
+    <span class="alex-brush lg-font">from the evening of</span></br>
+    <span class="baskerville-regular capital-text lg-font">Friday 5th</span></br>
+    <span class="alex-brush lg-font">to</span></br>
+    <span class="baskerville-regular capital-text lg-font">Sunday 7th June 2026</span></br>
+    <span class="alex-brush lg-font">at</span></br>
+    <span class="baskerville-regular capital-text"><a href="https://maps.app.goo.gl/SR8cTgCmtWMvd39s8">Tonedale House</a></span>
+    <span class="baskerville-regular capital-text sm-font">Milverton Rd, Wellington, TA21 0EZ</span>
+</div>
+
+<div class="rsvp-btn-container">
+    <a href="#rsvp" class="rsvp-here-btn baskerville-regular capital-text">
+        RSVP here
+    </a>
+</div>
+
+<div class="floral-bouquet">
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/floral-bouquet.png"/>
+</div>
+
+<div class="baskerville-regular">
+    <p>
+        We are gathering our closest family and friends for a quiet, relaxed wedding weekend at Tonedale House. It will be a chance to slow down, spend proper time together, and celebrate our marriage in a relaxed and unhurried way.
+    </p>
+    <p>A couple weeks later on <strong>Saturday 20th June</strong>, we will be hosting an evening of drinks, dancing, and cake, to mark our marriage with a wider group of guests. We would be delighted if you could join us again.</p>
+    <p>
+        Below you'll find everything you need to know, along with the RSVP to let us know which parts you'll be joining us for.
+    </p>
+</div>
+
+<div class="floral-page-split">
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/floral-page-split.png"/>
+</div>
+
+<div class="baskerville-regular capital-text lg-font">
+    The Weekend at a Glance
+</div>
+<div class="baskerville-regular">
+    <p class="capital-text">Friday</p>
+    <hr>
+    <p>We understand that most people will be finishing work on the Friday afternoon, so there's no rush come on over when you can, everyone is welcome anytime after 4pm.</p>
+    <p>Weather and volunteer permitting we might play some garden games of rounders and badminton.</p>
+    <p>We'll gather for a BBQ around 6:30pm, followed by a pub quiz hosted by the Luffman patriart and a enjoy a relaxed evening together with drinks and fun.</p>
+
+    <p class="capital-text">Saturday</p>
+    <hr>
+    <p>We hope you enjoy a leisurely morning. Help yourself to some breakfast, or if your're looking for something a little more indulgent, wander through the secret garden door to <a href="https://www.braziercoffeeroasters.co.uk/file/brazier_food_menu_2025_spring_outlined.pdf">Brazier Café</a>.</p>
+    <p>The ceremony begins at <strong>12 noon</strong>, and, weather permitting will be held outside, please bring appropriate footwear. This will be followed by a banquet-style feast, speeches, cake and an evening of celebration.</p>
+
+    <p class="capital-text">Sunday</p>
+    <hr>
+    <p>We imagine coffee will be involved and a slow morning. Packing up and goodbyes before departure latest by 4pm.</p>
+</div>
+
+<div class="floral-page-split">
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/floral-page-split.png"/>
+</div>
+
+<div class="baskerville-regular capital-text lg-font">
+    What do I need to bring?
+</div>
+<div class="baskerville-regular">
+    <p>Just yourself!</p>
+    <p>The venue provide linens, towels and soap. You'll need to bring your own shampoo and toothpaste.</p>
+    <p>There is a hottub on site, so if you fancy a dip don't forget your costume!</p>
+    <p>We will be providing a selection of soft drinks, wine, fizz, beer, ale, and cider throughout the weekend. If you have a particular drink of choice, please feel free to bring it along.</p>
+</div>
+
+<div class="floral-page-split">
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/floral-page-split.png"/>
+</div>
+
+<div class="baskerville-regular capital-text lg-font">
+    How do I get to the venue?
+</div>
+<div class="baskerville-regular">
+    <p>The weekend will be held at Tonedale House, in the suburb of Tonedale in Wellington. Near the house go slowly over the metal railway bridge as the House driveway is an immediate left as you come off.</p>
+    <p><strong>Address:</strong> <a href="https://maps.app.goo.gl/wnWc1bSWb8ah7ysDA">Tonedale House, Wellington, Somerset, TA21 0EZ</a></p>
+    <p><strong>Precise what3words to the driveway entrance:</strong> <a href="https://w3w.co/lyricist.node.grape">///lyricist.node.grape</a></p>
+</div>
+
+<div class="floral-page-split">
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/floral-page-split.png"/>
+</div>
+
+<div class="baskerville-regular capital-text lg-font">
+    Is there parking?
+</div>
+<div class="baskerville-regular">
+    <p>There is parking available for about 12 cars on site. We kindly ask that the courtyard is kept clear for suppliers. Additional on-street parking is available along <a href="https://maps.app.goo.gl/HJZfptW99X5ZLYUo7">Millstream Gardens & Weavers Reach road</a>.</p>
+</div>
+
+<div class="baskerville-regular capital-text lg-font">
+    Is there acommodation?
+</div>
+<div class="baskerville-regular">
+    <p>There is parking available for about 12 cars on site. We kindly ask that the courtyard is kept clear for suppliers. Additional on-street parking is available along <a href="https://maps.app.goo.gl/HJZfptW99X5ZLYUo7">Millstream Gardens & Weavers Reach road</a>.</p>
+</div>
+
+<div class="baskerville-regular capital-text lg-font">
+    Is there a dress code?
+</div>
+<div class="baskerville-regular">
+    <p>There is parking available for about 12 cars on site. We kindly ask that the courtyard is kept clear for suppliers. Additional on-street parking is available along <a href="https://maps.app.goo.gl/HJZfptW99X5ZLYUo7">Millstream Gardens & Weavers Reach road</a>.</p>
+</div>
+
+<?php
+get_footer(); 
+?>
